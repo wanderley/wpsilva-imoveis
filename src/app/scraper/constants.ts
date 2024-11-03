b517428a68dc689c244b3d4bf546a99732a9af45
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2024-11-03 14:44:48 -0800

    add vivaleiloes

--- src/app/scraper/constants.ts
+++ src/app/scraper/constants.ts
@@ -1,4 +1,5 @@
 export const SCRAPERS: { [key: number]: string } = {
   0: "www.wspleiloes.com.br",
   1: "www.agsleiloes.com.br",
+  2: "www.vivaleiloes.com.br",
 };
--- src/services/scraper/index.ts
+++ src/services/scraper/index.ts
@@ -1,8 +1,11 @@
 import { Scraper } from "@/services/scraper/scraper";
-import { Agsleiloes } from "@/services/scraper/scrapers/agsleiloes";
+import {
+  Agsleiloes,
+  VivaLeiloes,
+} from "@/services/scraper/scrapers/degrau-publicidade";
 import { Wspleiloes } from "@/services/scraper/scrapers/wspleiloes";
 
-export const scrapers: Scraper[] = [Wspleiloes, Agsleiloes];
+export const scrapers: Scraper[] = [Wspleiloes, Agsleiloes, VivaLeiloes];
 
 export function getScraper(scraperID: string): Scraper | undefined {
   return scrapers.find((scraper) => scraper.url === scraperID);
