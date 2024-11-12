37043a0ba5a1011724dd1f82ba7b3a3e0aed9bc7
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2024-11-12 06:55:38 -0800

    fix puppeteer to run on docker

--- src/services/scraper/actions.ts
+++ src/services/scraper/actions.ts
@@ -21,7 +21,10 @@ export async function refreshScraps(scraperID: string): Promise<void> {
   if (!scraper) {
     throw new Error(`Scraper ${scraperID} not found`);
   }
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    // TODO: Remove this once we have a proper way to run the scraper
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   try {
     const page = await browser.newPage();
     await page.setViewport({ width: 1080, height: 1024 });
