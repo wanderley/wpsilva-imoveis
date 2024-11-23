73fe936357197f157a6b98b37c44715cfa23b9ec
Wanderley da Silva <wanderley.guimaraes@gmail.com>
2024-11-23 09:35:45 -0800

    add default db port

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,6 +7,7 @@ const connection = mysql.createPool({
   user: process.env.DB_USER!,
   password: process.env.DB_PASSWORD!,
   database: process.env.DB_NAME!,
+  port: Number(process.env.DB_PORT || 3306),
 });
 
 export const db = drizzle(connection, { schema, mode: "default" });
